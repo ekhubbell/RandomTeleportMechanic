85577175b3aef248e7da56bc5db5f5748abf7b92
Emma <ekhubbell@qu.edu>
2020-08-31 11:41:47 -0400

    Initial commit

---
+++ Assets/EventManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    public static event Action OnClick = delegate { };
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            OnClick();
+        }
+    }
+}
---
+++ Assets/Teleport.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public Vector2[] positions;
+    int currentPosition;
+    int newPosition;
+
+    void Start()
+    {
+        move();
+        EventManager.OnClick += move;
+    }
+
+    void move()
+    {
+        
+        newPosition = Random.Range(0, positions.Length);
+        if(newPosition != currentPosition)
+        {
+            gameObject.transform.position = positions[newPosition];
+            currentPosition = newPosition;
+        }
+        else
+        {
+            move();
+        }
+    }
+}
